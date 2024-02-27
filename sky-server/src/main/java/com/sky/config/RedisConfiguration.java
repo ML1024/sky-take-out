42d1ec182d3aaf9077fe8205e8f000d7720f1cb5
mmmml <lucy83844@gmail.com>
2024-02-28 00:18:54 +0800

    Redis配置

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,26 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+        log.info("开始创建redis模板对象");
+
+        RedisTemplate redisTemplate =  new RedisTemplate<>();
+        //设置redis的连接工厂对象
+        //redis的连接工厂对象不用我们自己创建，pom.xml引入的redis包，已经把工厂对象创建好，并放到spring容器中了
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //设置redis key的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
